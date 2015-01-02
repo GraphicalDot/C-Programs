87d2327724eaec29554519c1ad219ebb9bc5f25a
kaali-python <saurav.verma@outlook.com>
2015-01-02 18:18:27 +0530

    commit 4

---
+++ Pointers/char_pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(void){
+
+	char my_other_string[100];
+	char my_string[40] = "Kaali";
+	int i = 0;
+	strcpy(my_other_string, "vande matram");
+	puts(my_other_string);
+
+	return ;
+
+};
